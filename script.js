082601729a51c432b8cc40ceafb09c206aa2d7ac
“cristopher” <curibe26@aoiths.org>
2025-12-12 13:51:10 -0500

    update

--- script.js
+++ script.js
@@ -20,16 +20,88 @@ let
 /////////////////////////////////////////////////////
 
 ////////// MAKE YOUR UPDATES WITHIN THIS SECTION //// 
-Admin.splice(4, 1);  
+Admin.splice(4, 2);  
 Admin.splice(3, 1);
-Admin.push("simpkens")
+Admin.splice(0, 1);
+Admin.push("simpkins")
 
+
+
+
+Office.push("mr.marinez")
+Office.push("mike")
+Office.push("tirido")
+
+Counselor.push("slater")
+Counselor.push("cooper")
 Counselor.push("molina")
 
+Counselor.splice(0, 2);  
 
 
 
 
+CTE.push("pelzer")
+CTE.splice(1, 2);
+CTE.splice(1, );
+CTE.push("pelzer")
+CTE.push("piere")
+CTE.push("frazer")
+CTE.push("ramswak")
+CTE.push("shams")
+
+History.splice(0, 2); 
+History.splice(0, 2); 
+History.push("urena")
+History.push("rodriguez")
+History.push("amell")
+Maths = Maths.filter(name => name !== "Ventimiglia");
+
+Maths.push("wong")
+Maths.push("steel")
+Maths.push("minto")
+Maths.push("koelsh")
+Maths.push("forth")
+
+
+Gym.splice(1, 2);
+Gym.splice(1, 2);
+Gym.push("Macalary")
+
+
+
+
+
+Dean.splice(Dean.indexOf("Vaduva"), 1);
+
+Language.splice(0, Language.length);
+Language.push("valejo")
+Language.push("weaver")
+Language.push("nowakowski")
+
+Science.splice(1, 2);
+Science.splice(1, 2);
+Science.splice(0, );
+Science.push("Simpkins")
+Science.push("Lynch")
+Science.push("Davies")
+Science.push("Kabir")
+
+English.splice(0, 8);
+English.push("Grant")
+English.push("Taveras")
+English.push("Sherman")
+English.push("Gomez")
+English.push("Slater")
+English.push("Grizell")
+
+Support.splice(0, 2);
+Support.unshift("david")
+Support.push("Jay")
+Support.push("Griffen")
+
+SSC.splice(0, 8);
+SSC.push("ricardo")
 
 
 
